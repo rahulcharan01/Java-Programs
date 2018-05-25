4294d1007c65043195db11f2019eccb7a70de994
Rahul Charan <rahulcharan0190@outlook.com>
2018-05-25 09:08:22 +0530

    Simple Addition Program Without User Input

---
+++ HelloWorld.java
@@ -0,0 +1,12 @@
+package com.rchitect.java.simpleprograms;
+
+/**
+ * @author Rahul
+ *
+ */
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
+}
